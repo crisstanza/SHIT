8405b68bd90a81d1fd1f9f13612b7b871de34213
crisstanza <crisstanza@home>
2012-09-22 19:28:26 -0300

    New actions

---
+++ add-all.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. shit-config.sh
+clear
+
+echo ""
+pwd
+
+for branch in ${SHIT_USE_BRANCH[@]} ; do
+
+	branch_name=${branch%%=*}
+	branch_use=${branch##*=}
+
+	if [ "$branch_use" -eq "1" ]; then
+		echo ""
+		cd $branch_name
+		pwd
+		git add .
+		cd ..
+	fi
+
+done
+
+echo ""
---
+++ commit-all.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. shit-config.sh
+clear
+
+echo ""
+pwd
+
+for branch in ${SHIT_USE_BRANCH[@]} ; do
+
+	branch_name=${branch%%=*}
+	branch_use=${branch##*=}
+
+	if [ "$branch_use" -eq "1" ]; then
+		echo ""
+		cd $branch_name
+		pwd
+		git commit -m "$1"
+		cd ..
+	fi
+
+done
+
+echo ""
