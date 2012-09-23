4c5b39f37adff40521c8d8d9d4b56706fa26d463
crisstanza <crisstanza@work>
2012-09-23 19:37:09 -0300

    Update to specify branches' names in a associative array

--- clone-all.sh
+++ clone-all.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
 . shit-config.sh
-
 clear
 
 echo ""
 
-if [ "$SHIT_USE_BRANCH_MASTER" -eq "1" ]; then
-	# git checkout --orphan zero
-	# git rm -rf .
-	# echo .DS_Store > .gitignore
-	# git add .
-	# git commit -m "Initial commit"
-	# git push origin zero
-	git clone git@github.com:$SHIT_USER/$SHIT_REPOSITORY.git master
-	echo ""
-fi
+for branch in ${SHIT_USE_BRANCH[@]} ; do
 
-if [ "$SHIT_USE_BRANCH_GH_PAGES" -eq "1" ]; then
-	git clone git@github.com:$SHIT_USER/$SHIT_REPOSITORY.git -b gh-pages gh-pages
-	echo ""
-fi
+	branch_name=${branch%%=*}
+	branch_use=${branch##*=}
 
-if [ "$SHIT_USE_BRANCH_ZERO" -eq "1" ]; then
-	git clone git@github.com:$SHIT_USER/$SHIT_REPOSITORY.git -b zero zero
-	echo ""
-fi
+	if [ "$branch_use" -eq "1" ]; then
+		git clone git@github.com:$SHIT_USER/$SHIT_REPOSITORY.git -b $branch_name $branch_name
+		echo ""
+	fi
+
+done
+
+# git checkout --orphan zero
+# git rm -rf .
+# echo .DS_Store > .gitignore
+# git add .
+# git commit -m "Initial commit"
+# git push origin zero
