56390d4bdfc138aa7c9281f40dd7f82a251eed29
crisstanza <crisstanza@work>
2012-09-28 18:37:40 -0300

    Help file, to remember most common commands

---
+++ sh/help.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ..
+. shit-config.sh
+clear
+
+echo ""
+
+
+echo "Delete a local branch:     git branch -D <LOCAL BRANCH'S NAME>"
+echo "Delete a remote branch:    git push origin --delete <REMOTE BRANCH'S NAME>"
+
+echo ""
